10a9801822323e604163ee9683e5de369c811246
dpdp0801 <taeyangpark0801@gmail.com>
2025-04-28 00:20:51 -0700

    Finalize Tailwind + shadcn/ui setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
+import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
-export default function App({ Component, pageProps }) {
+
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
-    <SessionProvider session={pageProps.session}>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
   );
 }
+
+export default MyApp;
