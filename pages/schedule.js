10a60e88226cd50ce943f4b9f1f36006893cbd42
dpdp0801 <taeyangpark0801@gmail.com>
2025-04-28 03:35:51 -0700

    Restore deleted files: schema.prisma, rules.js, schedule.js

---
+++ pages/schedule.js
@@ -0,0 +1,8 @@
+export default function Schedule() {
+  return (
+    <div className="container py-12">
+      <h1 className="text-4xl font-bold mb-6">Tournament Schedule</h1>
+      <p>Schedule page content will go here.</p>
+    </div>
+  );
+} 
