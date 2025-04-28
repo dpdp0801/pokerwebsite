cba3359650df0e6139ff93d3caee6d3519bba00f
dpdp0801 <taeyangpark0801@gmail.com>
2025-04-28 12:30:41 -0700

    Fix 404 page issues in Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  experimental: {
+    instrumentationHook: false
+  }
+} 
